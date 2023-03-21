efc8724d7125bf85023e4e09965fe39b181494b8
RAMORA <dereckongaro1086@gmail.com>
2023-03-21 14:08:57 +0300

    django_forms_and_database

---
+++ django_forms_and_database/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=30, blank=False, null=False)
+    email = models.CharField(max_length=30, blank=False, null=False)
+    idnum = models.IntegerField(max_length=30, blank=False, null=False)
+
+
+
+def __str__(self):
+    return self.name
+
+
+
+
